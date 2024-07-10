05dfd3014a82953923277b5965cfae3f8ba92c2e
Bassam Osama <bassamosamabsa@gmail.com>
2024-07-10 22:08:37 +0300

    Fourth Commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 "use strict";
 
-// var username = localStorage.getItem("sessionUsername");
-// if (userName == null) {
-//   location.replace("https://" + location.hostname + "/index.html");
-// }
+// Function to get an element by ID and ensure it exists
+function getElement(id) {
+  return document.getElementById(id);
+}
 
 // All inputs
-var signUpName = document.getElementById("signUpName");
-var signUpEmail = document.getElementById("signUpEmail");
-var signUpPassword = document.getElementById("signUpPassword");
-var signInEmail = document.getElementById("signInEmail");
-var signInPassword = document.getElementById("signInPassword");
+var signUpName = getElement("signUpName");
+var signUpEmail = getElement("signUpEmail");
+var signUpPassword = getElement("signUpPassword");
+var signInEmail = getElement("signInEmail");
+var signInPassword = getElement("signInPassword");
 
 // To get base URL (localhost)
 var pathParts = location.pathname.split("/");
@@ -18,12 +18,12 @@ var baseURL = "";
 for (var i = 0; i < pathParts.length - 1; i++) {
   baseURL += "/" + pathParts[i];
 }
-console.log(baseURL);
+console.log("Base URL:", baseURL);
 
-// To say welcome in home page
+// To say welcome on the home page
 var userName = localStorage.getItem("sessionUsername");
 if (userName) {
-  var userNameElement = document.getElementById("userName");
+  var userNameElement = getElement("userName");
   if (userNameElement) {
     userNameElement.innerHTML = "Welcome " + userName;
   }
@@ -36,10 +36,13 @@ var signUpArray = localStorage.getItem("users")
 
 // Check if sign up inputs are empty
 function isEmpty() {
-  return !(
-    signUpName.value === "" ||
-    signUpEmail.value === "" ||
-    signUpPassword.value === ""
+  return (
+    signUpName &&
+    signUpEmail &&
+    signUpPassword &&
+    signUpName.value !== "" &&
+    signUpEmail.value !== "" &&
+    signUpPassword.value !== ""
   );
 }
 
@@ -53,13 +56,13 @@ function isEmailExist() {
 // Sign up function
 function signUp() {
   if (!isEmpty()) {
-    document.getElementById("Exist").innerHTML =
+    getElement("Exist").innerHTML =
       '<span class="text-danger m-3">All Inputs Are Required</span>';
     return false;
   }
 
   if (isEmailExist()) {
-    document.getElementById("Exist").innerHTML =
+    getElement("Exist").innerHTML =
       '<span class="text-danger m-3">Email Already Exists</span>';
     return false;
   }
@@ -73,20 +76,25 @@ function signUp() {
 
   signUpArray.push(signUp);
   localStorage.setItem("users", JSON.stringify(signUpArray));
-  document.getElementById("Exist").innerHTML =
+  getElement("Exist").innerHTML =
     '<span class="text-success m-3">Success</span>';
   return true;
 }
 
 // Check if login inputs are empty
 function isLoginEmpty() {
-  return !(signInEmail.value === "" || signInPassword.value === "");
+  return (
+    signInEmail &&
+    signInPassword &&
+    signInEmail.value !== "" &&
+    signInPassword.value !== ""
+  );
 }
 
 // Log in function
 function logIn() {
   if (!isLoginEmpty()) {
-    document.getElementById("wrong").innerHTML =
+    getElement("wrong").innerHTML =
       '<span class="text-danger m-3">All Inputs Are Required</span>';
     return false;
   }
@@ -102,14 +110,15 @@ function logIn() {
       localStorage.setItem("sessionUsername", signUpArray[i].name);
       var redirectURL =
         baseURL === "/"
-          ? "https://" + location.hostname + "/home.html"
+          ? "http://" + location.hostname + ":5500/home.html"
           : baseURL + "/home.html";
+      console.log("Redirect URL:", redirectURL); // Debug logging
       location.replace(redirectURL);
       return;
     }
   }
 
-  document.getElementById("wrong").innerHTML =
+  getElement("wrong").innerHTML =
     '<span class="p-2 text-danger">Incorrect Email Or Password</span>';
 }
 
